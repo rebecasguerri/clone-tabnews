3a4a116d80638396424d9adc7c652749bb83da5d
Rebeca Sguerri <rebecasguerri@gmail.com>
2024-06-12 18:20:02 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Jesus, eu amo voce. se o senhor me ama, da uma risadinha!</h1>
+    return <h1>Jesus, eu amo voce. se o senhor me ama, da uma risadinha! 😎 </h1>
 }
 
 export default Home;
