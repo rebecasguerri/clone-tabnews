27e23a26698ec20720168e3965c48001f55a03e6
Rebeca Sguerri <rebecasguerri@gmail.com>
2024-06-14 03:08:46 +0000

    alterando a mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Jesus, eu amo voce. se o senhor me ama, da uma risadinha! 😎 </h1>
+    return <h1>Day voce mora no meu coração, te amo! </h1>
 }
 
 export default Home;
